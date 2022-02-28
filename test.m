6a537f2f16f2a46f19b43f3cec42b7d3ec4d2def
crissmath <crirabe@upv.es>
2022-02-28 20:22:50 +0100

    update test

--- test.m
+++ test.m
@@ -3,30 +3,39 @@ clc
 clear all
 close all
 
-vart_blis = 0;
-
-n_kernels =1;
+vart_blis = 1;
+n_kernels = 3;  
 
 m = 1792;
 n = 1536;
 k = 1024;
-
-MC = 128;
-NC = 2048;
-KC = 1024;
 
 MR = 4;
 KR = 4;
 
 if vart_blis == 0
     fprintf(" **** B3C2A0 ****\n");
-    for F=1:n_kernels  
+    for F=1:n_kernels
+        if F == 1
+            MC = 128; NC = 2048; KC = 1024;
+        elseif F == 2
+            MC = 256; NC = 1024; KC = 2048;
+        elseif F == 3
+            MC = 384; NC = 682;  KC = 3072;
+        end    
         [PackBc, PackCc, UnpackCc, CopyBr, StreamAr, StreamBr, StreamCr, ...
         BrmemL1, BcmemL3, CcmemL2 ] = gemm_blis_B3C2A0( m, n, k, MC, NC, KC, MR, KR*F );
     end
 elseif vart_blis == 1
     fprintf(" **** C3B2A0 ****\n");
     for F=1:n_kernels 
+        if F == 1
+            MC = 128; NC = 2048; KC = 1024;
+        elseif F == 2
+            MC = 256; NC = 1024; KC = 2048;
+        elseif F == 3
+            MC = 384; NC = 682;  KC = 3072;
+        end
         [PackBc, PackCc, UnpackCc, CopyCr, StreamAr, StreamBr, StreamCr, ...
         BrmemL1, BcmemL3, CcmemL2 ] = gemm_blis_C3B2A0( m, n, k, MC, NC, KC, MR, KR*F );
     end    
