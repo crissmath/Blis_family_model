a1e6b2439fade47653c67bbd90c252ae23d68549
crissmath <crirabe@upv.es>
2022-03-08 17:56:01 +0100

    Model complete v1

--- mem_model_gap8.m
+++ mem_model_gap8.m
@@ -1,7 +1,7 @@
 function [MC, NC, KC, Mem_L1, Mem_L2, Mem_L3, Mem_L1_use, Mem_L2_use, ...
      Mem_L3_use] = mem_model_gap8(variant, m, n, k, MR, NR, KR)
 
-%variant = 'C3B2A0'; %'B3A2C0' ; %'B3C2A0';
+     %variant = 'C3B2A0'; %'B3A2C0' ; %'B3C2A0';
 %m = 1792; 
 %n = 1536;
 %k = 1024;
@@ -19,55 +19,7 @@ Mem_L1 = 16*KiB;
 Mem_L2 = 512*KiB;
 Mem_L3 = 8*MiB;
 
-if variant == 'B3C2A0'
-    % L3
-    A_L3  = m*k;
-    B_L3  = k*n;
-    C_L3  = m*n;
-    %
-    % L2
-    Cg_L2  = m*n;
-    %
-    % L2+test
-    C_2_L2 = m*n;
-    Ag     = m*k;
-    Bg     = k*n;
-    %
-    % Memory use include buffers
-    Mem_L3_use = A_L3 + B_L3 + C_L3;
-    if(Mem_L3_use > Mem_L3)
-        fprintf("Use of L3 us exeded..\n");
-    else 
-        Mem_L3 = Mem_L3-Mem_L3_use;
-    end
-    %================================================
-    % Calculate mc, nc, kc
-    %================================================
-    % Br --> L1(KR*NC)
-    % NC = L1/KR
-    NC = 4*floor((Mem_L1/KR)/4);
-    % Cc --> L2(MC*NC) 
-    MC = 4*floor((Mem_L2/min(n,NC))/4); % if NC>n  
-    % Bc --> L3(KC*NC)
-    KC = 4*floor((Mem_L3/min(n,NC))/4);
-    %================================================
-    % Check space in memory
-    % Buffer for data 
-    % L3 
-    Bc_L3 = KC*NC;
-    % L2
-    Brtmp_L2 = KR * NC;
-    Artmp_L2 = MR * KR;
-    % L1
-    Br_L1 = KR * NC;
-    %
-    % Memory use include buffer
-    Mem_L1_use = Br_L1;
-    Mem_L2_use = Cg_L2; % add L2+test: 
-    Mem_L1 = Mem_L1-Mem_L1_use;
-    Mem_L2 = Mem_L2-Mem_L2_use;
-
-elseif variant == 'B3A2C0'
+if variant == 'B3A2C0'
     % L3
     A_L3  = m * k;
     B_L3  = k * n;
@@ -116,53 +68,102 @@ elseif variant == 'B3A2C0'
     %L1 = L1-Mem_L1_use;
     %L2 = L2-L2_use;
 
+elseif variant == 'B3C2A0'
+    % L3
+    A_L3  = m*k;
+    B_L3  = k*n;
+    C_L3  = m*n;
+    %
+    % L2
+    Cg_L2  = m*n;
+    %
+    % L2+test
+    C_2_L2 = m*n;
+    Ag     = m*k;
+    Bg     = k*n;
+    %
+    % Memory use include buffers
+    Mem_L3_use = A_L3 + B_L3 + C_L3;
+    if(Mem_L3_use > Mem_L3)
+        fprintf("Use of L3 us exeded..\n");
+    else 
+        Mem_L3 = Mem_L3-Mem_L3_use;
+    end
+    %================================================
+    % Calculate mc, nc, kc
+    %================================================
+    % Br --> L1(KR*NC)
+    % NC = L1/KR
+    NC = 4*floor((Mem_L1/KR)/4);
+    % Cc --> L2(MC*NC) 
+    MC = 4*floor((Mem_L2/min(n,NC))/4); % if NC>n  
+    % Bc --> L3(KC*NC)
+    KC = 4*floor((Mem_L3/min(n,NC))/4);
+    %================================================
+    % Check space in memory
+    % Buffer for data 
+    % L3 
+    Bc_L3 = KC * NC;
+    % L2
+    Brtmp_L2 = KR * NC;
+    Artmp_L2 = MR * KR;
+    % L1
+    Br_L1 = KR * NC;
+    %
+    % Memory use include buffer
+    Mem_L1_use = Br_L1;
+    Mem_L2_use = Cg_L2; % add L2+test: 
+    Mem_L1 = Mem_L1 - Mem_L1_use;
+    Mem_L2 = Mem_L2 - Mem_L2_use;
+    
 elseif variant == 'C3B2A0'
-        % L3
-        A_L3  = m * k;
-        B_L3  = k * n;
-        C_L3  = m * n;
-        %
-        % L2
-        Bg_L2  = m * n;
-        %
-        % L2+test
-        B_2_L2 = m * n;
-        Ag     = m * k;
-        Bg     = k * n;
-        %--------------------------------
-        %   Memory use include buffers
-        %--------------------------------
-        Mem_L3_use = A_L3 + B_L3 + C_L3;
-        if(Mem_L3_use > Mem_L3)
-            fprintf(" Exceeds L3 capacity : %d Kib > %d KiB\n",L3_use, Mem_L3);
-        else 
-            Mem_L3 = Mem_L3 - Mem_L3_use;
-        end
-        %================================================
-        %       C3B2A0 : mc, nc, kc
-        %================================================
-        % Cr --> L1(MR*NC) --> NC = L1/MR
-        NC = 4 * floor((Mem_L1 / MR) / 4);
-        % Bc --> L2(KC*NC)
-        KC = 4 * floor((Mem_L2 / min(n,NC)) / 4);
-        % Cc --> L3(MC*NC)
-        MC = 4 * floor((Mem_L3 / min(n,NC)) / 4);
-        %================================================
-        % Check space in memory
-        % Buffer for data 
-        % L3 
-        Bc_L3 = KC * NC;
-        % L2
-        Brtmp_L2 = KR * NC; % dont include temporal buffers
-        Artmp_L2 = MR * KR;
-        % L1
-        Cr_L1 = KR * NC;
-        %
-        % Memory use include buffer
-        Mem_L1_use = Cr_L1;
-        Mem_L2_use = B_2_L2;
-        Mem_L1 = Mem_L1 - Mem_L1_use;
-        Mem_L2 = Mem_L2 - Mem_L2_use;
+    % L3
+    A_L3  = m * k;
+    B_L3  = k * n;
+    C_L3  = m * n;
+    %
+    % L2
+    Bg_L2  = m * n;
+    %
+    % L2+test
+    B_2_L2 = m * n;
+    Ag     = m * k;
+    Bg     = k * n;
+    %--------------------------------
+    %   Memory use include buffers
+    %--------------------------------
+    Mem_L3_use = A_L3 + B_L3 + C_L3;
+    if(Mem_L3_use > Mem_L3)
+        fprintf(" Exceeds L3 capacity : %d Kib > %d KiB\n",L3_use, Mem_L3);
+    else 
+        Mem_L3 = Mem_L3 - Mem_L3_use;
+    end
+    %================================================
+    %       C3B2A0 : mc, nc, kc
+    %================================================
+    % Cr --> L1(MR*NC) --> NC = L1/MR
+    NC = 4 * floor((Mem_L1 / MR) / 4);
+    % Bc --> L2(KC*NC)
+    KC = 4 * floor((Mem_L2 / min(n,NC)) / 4);
+    % Cc --> L3(MC*NC)
+    MC = 4 * floor((Mem_L3 / min(n,NC)) / 4);
+    %================================================
+    % Check space in memory
+    % Buffer for data 
+    % L3 
+    Bc_L3 = KC * NC;
+    % L2
+    Brtmp_L2 = KR * NC; % dont include temporal buffers
+    Artmp_L2 = MR * KR;
+    % L1
+    Cr_L1 = KR * NC;
+    %
+    % Memory use include buffer
+    Mem_L1_use = Cr_L1;
+    Mem_L2_use = B_2_L2;
+    Mem_L1 = Mem_L1 - Mem_L1_use;
+    Mem_L2 = Mem_L2 - Mem_L2_use;
+%
 end
 
 fprintf("------------------------------------------------\n");
@@ -177,16 +178,3 @@ fprintf("Free:          %3.2e       %6.2e       %6.2e\n", Mem_L1, Mem_L2, Mem_L3
 
 %
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
--- test.m
+++ test.m
@@ -4,56 +4,48 @@
 % close all
 
 %% var_blis = 'B3C2A0';
-var_blis = 'C3B2A0';
-
 m = 1792;
 n = 1536;
 k = 1024;
 
 MR = 4;
 KR = 4;
-NR = 12;
+NR = 4;
 
-%MR = 8;
-%KR = 8;
-%NR = 8;
-%MC = 128; NC = 128; KC = 128;
-
-%% if var_blis == 'B3C2A0'
-    fprintf(" **** B3C2A0 ****\n");
-        if MR==4 && KR==4 
-            MC = 128; NC = 2048; KC = 1024;
-        elseif MR==4 && KR==8
-            MC = 256; NC = 1024; KC = 2048;
-        elseif MR==4 && KR==12
-            MC = 384; NC = 682;  KC = 3072;
-        end    
-        %% MC = 128; NC = 128; KC = 128;
-        [PackBc, PackCc, UnpackCc, CopyBr, StreamAr, StreamBr, StreamCr, ...
-         BrmemL1, BcmemL3, CcmemL2 ] = gemm_blis_B3C2A0( m, n, k, MC, NC, KC, MR, KR);
-%% elseif var_blis == 'C3B2A0'
-    fprintf(" **** C3B2A0 ****\n");
-        if MR==4 && KR==4 
-            MC = 1024; NC = 2048; KC = 128;
-        elseif MR==4 && KR==8
-            MC = 2048; NC = 1024; KC = 256;
-        elseif MR==4 && KR==12
-            MC = 3072; NC = 682;  KC = 384;
-        end
-        %% MC = 128; NC = 128; KC = 128;
-        [PackBc, PackCc, UnpackCc, CopyCr, StreamAr, StreamBc, StreamCc, ...
-         CrmemL1, CcmemL3, BcmemL2 ] = gemm_blis_C3B2A0( m, n, k, MC, NC, KC, MR, KR );
 %% elseif var_blis == 'B3A2C0'
     fprintf(" **** B3A2C0 ****\n");
-        if MR==4 && NR==4 
-            MC = 128; NC = 1024; KC = 2048;
-        elseif MR==4 && NR==8
-            MC = 256; NC = 2048; KC = 1024;
-        elseif MR==4 && NR==12
-            MC = 384; NC = 3072;  KC = 682;
-        end
-        %% MC = 128; NC = 128; KC = 128;
-        [PackBc, PackAc, CopyBr, StreamAc, StreamBr, StreamCr, ...
-        BrmemL1, BcmemL3, AcmemL2 ] = gemm_blis_B3A2C0( m, n, k, MC, NC, KC, MR, NR);
-%% end 
+    % get parameters : mc, nc, kc
+    [MC, NC, KC, Mem_L1, Mem_L2, Mem_L3, Mem_L1_use, Mem_L2_use, ...
+    Mem_L3_use] = mem_model_gap8('B3A2C0', m, n, k, MR, NR, KR);
+
+    % model
+    [PackBc, PackAc, CopyBr, StreamAc, StreamBr, StreamCr, ...
+    BrmemL1, BcmemL3, AcmemL2 ] = gemm_blis_B3A2C0( m, n, k, MC, NC, KC, MR, NR);
+%% 
+
+%% if var_blis == 'B3C2A0'
+    fprintf(" **** B3C2A0 ****\n");    
+    % get parameters : mc, nc, kc
+    [MC, NC, KC, Mem_L1, Mem_L2, Mem_L3, Mem_L1_use, Mem_L2_use, ...
+    Mem_L3_use] = mem_model_gap8('B3C2A0', m, n, k, MR, NR, KR);
+
+    %model
+    [PackBc, PackCc, UnpackCc, CopyBr, StreamAr, StreamBr, StreamCr, ...
+    BrmemL1, BcmemL3, CcmemL2 ] = gemm_blis_B3C2A0( m, n, k, MC, NC, KC, MR, KR);
+
+%% elseif var_blis == 'C3B2A0'
+    fprintf(" **** C3B2A0 ****\n");
+    % Get parameters : mc, nc, kc
+    [MC, NC, KC, Mem_L1, Mem_L2, Mem_L3, Mem_L1_use, Mem_L2_use, ...
+    Mem_L3_use] = mem_model_gap8('C3B2A0', m, n, k, MR, NR, KR);
+        
+    % model  
+    [PackBc, PackCc, UnpackCc, CopyCr, StreamAr, StreamBc, StreamCc, ...
+    CrmemL1, CcmemL3, BcmemL2 ] = gemm_blis_C3B2A0( m, n, k, MC, NC, KC, MR, KR );
+%% end
+ 
+
+
+
+
 
