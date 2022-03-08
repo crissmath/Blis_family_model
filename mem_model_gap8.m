9bd0a7190681c6f174bac1b39a31e8bdd19a6f10
crissmath <crirabe@upv.es>
2022-03-08 16:02:32 +0100

    Add function to calculate diferent variats

--- mem_model_gap8.m
+++ mem_model_gap8.m
@@ -1,4 +1,4 @@
-function[MC, NC, KC, Mem_L1, Mem_L2, Mem_L3, Mem_L1_use, Mem_L2_use, ...
+function [MC, NC, KC, Mem_L1, Mem_L2, Mem_L3, Mem_L1_use, Mem_L2_use, ...
      Mem_L3_use] = mem_model_gap8(variant, m, n, k, MR, NR, KR)
 
 %variant = 'C3B2A0'; %'B3A2C0' ; %'B3C2A0';
@@ -45,41 +45,41 @@ if variant == 'B3C2A0'
     %================================================
     % Br --> L1(KR*NC)
     % NC = L1/KR
-    NC = Mem_L1/KR;
-    % Cc --> L2(MC*NC)
-    MC = Mem_L2/NC;
+    NC = 4*floor((Mem_L1/KR)/4);
+    % Cc --> L2(MC*NC) 
+    MC = 4*floor((Mem_L2/min(n,NC))/4); % if NC>n  
     % Bc --> L3(KC*NC)
-    KC = Mem_L3/NC;
+    KC = 4*floor((Mem_L3/min(n,NC))/4);
     %================================================
     % Check space in memory
     % Buffer for data 
     % L3 
     Bc_L3 = KC*NC;
     % L2
-    Brtmp_L2 = KR*NC;
-    Artmp_L2 = MR*KR;
+    Brtmp_L2 = KR * NC;
+    Artmp_L2 = MR * KR;
     % L1
-    Br_L1 = KR*NC;
+    Br_L1 = KR * NC;
     %
     % Memory use include buffer
     Mem_L1_use = Br_L1;
-    Mem_L2_use = Artmp_L2 + Brtmp_L2 + Cg_L2; % add L2+test 
-    %L1 = L1-L1_use;
-    %L2 = L2-L2_use;
+    Mem_L2_use = Cg_L2; % add L2+test: 
+    Mem_L1 = Mem_L1-Mem_L1_use;
+    Mem_L2 = Mem_L2-Mem_L2_use;
 
 elseif variant == 'B3A2C0'
     % L3
-    A_L3  = m*k;
-    B_L3  = k*n;
-    C_L3  = m*n;
+    A_L3  = m * k;
+    B_L3  = k * n;
+    C_L3  = m * n;
     %
     % L2
-    Ag_L2  = m*n;
+    Ag_L2  = m * n;
     %
     % L2+test
-    A_2_L2 = m*n;
-    Ag     = m*k;
-    Bg     = k*n;
+    A_2_L2 = m * n;
+    Ag     = m * k;
+    Bg     = k * n;
     %--------------------------------
     %   Memory use include buffers
     %--------------------------------
@@ -90,92 +90,93 @@ elseif variant == 'B3A2C0'
         Mem_L3 = Mem_L3 - Mem_L3_use;
     end
     %================================================
-    %       Calculate mc, nc, kc
+    %       B3A2C0: mc, nc, kc
     %================================================
     % Br --> L1(KC*NR) --> KC = L1/NR
-    KC = Mem_L1/NR;
+    KC = 4 * floor((Mem_L1 / NR) / 4);
     % Ac --> L2(MC*KC)
-    MC = Mem_L2/KC;
+    MC = 4 * floor((Mem_L2 / min(k,KC)) / 4);
     % Bc --> L3(KC*NC)
-    NC = Mem_L3/KC;
+    NC = 4 * floor((Mem_L3 / min(k,KC)) / 4);
     %================================================
     % Check space in memory
     % Buffer for data 
     % L3 
-    Bc_L3 = KC*NC;
+    Bc_L3 = KC * NC;
     % L2
-    Brtmp_L2 = KR*NC;
-    Artmp_L2 = MR*KR;
+    Brtmp_L2 = KR * NC;
+    Artmp_L2 = MR * KR;
     % L1
-    Br_L1 = KR*NC;
+    Br_L1 = KR * NC;
     %
     % Memory use include buffer
     Mem_L1_use = Br_L1;
+    Mem_L2_use = A_2_L2;
     L2_use = Artmp_L2 + Brtmp_L2; % add L2+test 
     %L1 = L1-Mem_L1_use;
     %L2 = L2-L2_use;
 
 elseif variant == 'C3B2A0'
         % L3
-        A_L3  = m*k;
-        B_L3  = k*n;
-        C_L3  = m*n;
+        A_L3  = m * k;
+        B_L3  = k * n;
+        C_L3  = m * n;
         %
         % L2
-        Bg_L2  = m*n;
+        Bg_L2  = m * n;
         %
         % L2+test
-        B_2_L2 = m*n;
-        Ag     = m*k;
-        Bg     = k*n;
+        B_2_L2 = m * n;
+        Ag     = m * k;
+        Bg     = k * n;
         %--------------------------------
         %   Memory use include buffers
         %--------------------------------
-        L3_use = A_L3 + B_L3 + C_L3;
-        if(L3_use > L3)
-            fprintf(" Exceeds L3 capacity : %d Kib > %d KiB\n",L3_use, L3);
+        Mem_L3_use = A_L3 + B_L3 + C_L3;
+        if(Mem_L3_use > Mem_L3)
+            fprintf(" Exceeds L3 capacity : %d Kib > %d KiB\n",L3_use, Mem_L3);
         else 
-            L3 = L3-L3_use;
+            Mem_L3 = Mem_L3 - Mem_L3_use;
         end
         %================================================
-        %       Calculate mc, nc, kc
+        %       C3B2A0 : mc, nc, kc
         %================================================
         % Cr --> L1(MR*NC) --> NC = L1/MR
-        NC = L1/MR;
+        NC = 4 * floor((Mem_L1 / MR) / 4);
         % Bc --> L2(KC*NC)
-        KC = L2/NC;
+        KC = 4 * floor((Mem_L2 / min(n,NC)) / 4);
         % Cc --> L3(MC*NC)
-        MC = L3/NC;
+        MC = 4 * floor((Mem_L3 / min(n,NC)) / 4);
         %================================================
         % Check space in memory
         % Buffer for data 
         % L3 
-        Bc_L3 = KC*NC;
+        Bc_L3 = KC * NC;
         % L2
-        Brtmp_L2 = KR*NC; % dont include temporal buffers
-        Artmp_L2 = MR*KR;
+        Brtmp_L2 = KR * NC; % dont include temporal buffers
+        Artmp_L2 = MR * KR;
         % L1
-        Cr_L1 = KR*NC;
+        Cr_L1 = KR * NC;
         %
         % Memory use include buffer
         Mem_L1_use = Cr_L1;
-        %L1 = L1-Mem_L1_use;
-        %L2 = L2-L2_use;
-%
+        Mem_L2_use = B_2_L2;
+        Mem_L1 = Mem_L1 - Mem_L1_use;
+        Mem_L2 = Mem_L2 - Mem_L2_use;
 end
 
 fprintf("------------------------------------------------\n");
-fprintf("GAP8 , INT8, variant : %s \n", variant);
+fprintf("variant : %s \n", variant);
 fprintf("------------------------------------------------\n");
 fprintf("mc = %d, nc = %d, kc = %d\n", MC, NC, KC);
 fprintf("------------------------------------------------\n");
-fprintf("Memory use\n")
+fprintf("Memory         L1          L2          L3\n");
 fprintf("------------------------------------------------\n");
-fprintf("L1             L2                  L3\n");
-fprintf("%3.2e      %6.2e           %6.2e\n", Mem_L1_use, Mem_L2_use, Mem_L3_use);
+fprintf("Used:          %3.2e       %6.2e       %6.2e\n", Mem_L1_use, Mem_L2_use, Mem_L3_use);
+fprintf("Free:          %3.2e       %6.2e       %6.2e\n", Mem_L1, Mem_L2, Mem_L3);
 
-
-
+%
+end
 
 
 
